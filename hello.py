dc3a079879694fe79c0c170e782404ea3613ee3b
Sebastian Wyngaard <swyngaard@users.noreply.github.com>
2021-04-12 15:27:24 +0200

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,23 @@
+import prefect
+from prefect import task, Flow
+from prefect.run_configs import KubernetesRun
+from prefect.storage import GitHub
+
+@task
+def say_hello():
+    logger = prefect.context.get("logger")
+    logger.info("Hello, Cloud!")
+
+with Flow("hello-flow") as flow:
+    say_hello()
+
+flow.run_config = KubernetesRun(image="prefecthq/prefect:0.14.15")
+
+flow.storage = GitHub(
+    repo="swyngaard/prefect-flows",              # name of repo
+    path="hello.py",                             # location of flow file in repo
+    #access_token_secret="GITHUB_ACCESS_TOKEN"   # name of personal access token secret
+)
+
+# Register the flow under the "tutorial" project
+flow.register(project_name="tutorial")
